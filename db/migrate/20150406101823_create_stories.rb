ffa32726602668a1a7578f7e0ff38eb4a66c2947
Samuel Richardson <sam@richardson.co.nz>
2015-04-08 14:27:21 +1000

    Use different datatype

--- db/migrate/20150406101823_create_stories.rb
+++ db/migrate/20150406101823_create_stories.rb
@@ -1,7 +1,7 @@
 class CreateStories < ActiveRecord::Migration
   def change
     create_table :stories do |t|
-      t.jsonb :details, null: false
+      t.json :details, null: false
       t.binary :content
       t.timestamps null: false
     end
