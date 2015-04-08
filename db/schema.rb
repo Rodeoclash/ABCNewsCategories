a5f4cc2852da66c4b09347eb0dcbc34e6b07dcd4
Samuel Richardson <sam@richardson.co.nz>
2015-04-08 14:28:49 +1000

    Use standard json datatype

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 20150406101823) do
   enable_extension "plpgsql"
 
   create_table "stories", force: :cascade do |t|
-    t.jsonb    "details",    null: false
+    t.json     "details",    null: false
     t.binary   "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
